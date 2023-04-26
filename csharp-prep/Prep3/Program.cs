d7250dc047c9a795a3a83efb26a539448a2060d8
Cyprian Ezeani <onyebuekeugochukwu757@gmail.com>
2023-04-26 16:26:46 +0200

    initial commit

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,40 @@
-using System;
+ using System;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep3 World!");
+    class GuessMyNunberGame {
+        static void Main() {
+
+          do {
+            int magicNumber = new Random().Next(1, 100);
+            int guessCount = 0;
+
+            Console.WriteLine("I'm thinking of a number between 1 and 100. Can you guess it?");
+
+            while (true) {
+                Console.WriteLine("Enter your guess: ");
+                int guess = Convert.ToInt32(Console.ReadLine());
+
+                guessCount++;
+
+                if (guess < magicNumber) {
+                    Console.WriteLine("Too low! Guess higher next time.");
+                } else if (guess > magicNumber) {
+                    Console.WriteLine("Too high! Guess lower next time.");
+
+                } else {
+                    Console.WriteLine("Congratulations! You guessed the magic number in {0} guesses.", guessCount);
+                    break;
+
+
+                }
+
+            } 
+
+            Console.WriteLine("Do you want to play again? (yes/no): ");
+
+        } while (Console.ReadLine().ToLower() == "yes");
+
     }
-}
+
+ }
+
+    
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,48 @@
 using System;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep4 World!");
+using System.Collections.Generic;
+
+using System.Linq;
+
+class Program {
+    static void Main(string[] args) {
+        List<double> numbers = new List<double>();
+
+        // Ask the user for a series of numbers and add them to the List
+        double input = 1.0;
+        while (input != 0) {
+            Console.Write("Enter a number (0 to stop): ");
+            input = Convert.ToDouble(Console.ReadLine());
+            if (input != 0) {
+                numbers.Add(input);
+            }
+        }
+
+        //  Compute the sum of the numbers in the list
+        double sum = numbers.Sum();
+
+        // Compute the average of the numbers in the list
+        double average = numbers.Average();
+
+        // Find the maximum value in the list
+        double max = numbers.Max();
+
+        // Find the positive number closest to zero in the list
+        double closestPositive = numbers.Where(n => n > 0).DefaultIfEmpty().OrderBy(n => Math.Abs(n)).First();
+
+        // Sort the numbers in the list and display the sorted list
+        numbers.Sort();
+        Console.Write("Sorted number: ");
+        foreach (double number in numbers) {
+            Console.Write(numbers + " ");
+        }
+        Console.WriteLine();
+
+        // output the results
+        Console.WriteLine("Sum: " + sum);
+        Console.WriteLine("Average: " + average);
+        Console.WriteLine("Max: " + max);
+        Console.WriteLine("Positive number closest to zero: " + closestPositive);
     }
+
 }
